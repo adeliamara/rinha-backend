132e3964e05210570bbd4bbf6f762eb56dede407
Adélia Mara <adeliamara13@gmail.com>
2023-09-17 12:38:10 -0300

    refactor: modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get '/people/count', to: 'people#count'
-  resources :people
+  get '/contagem-pessoas', to: 'people#count'
+  get '/pessoas', to: 'people#index'
+  get '/pessoas/:id', to: 'people#show'
+  post '/pessoas', to: 'people#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
